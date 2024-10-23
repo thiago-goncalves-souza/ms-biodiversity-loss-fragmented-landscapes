7f1e47ff62f5027e6a5b394de36cced6119ead35
Thiago Gonçalves-Souza <tgoncalves.souza@gmail.com>
2024-10-22 21:46:13 -0400

    Add files via upload

--- 05_pairwise_div_raref_samp_cov.R
+++ 05_pairwise_div_raref_samp_cov.R
@@ -15,7 +15,6 @@ renv::restore() # it requires installing the package renv
 library(vegan)
 library(tidyverse)
 library(ggplot2)
-library(fields)
 library(mobr)
 library(iNEXT)
 library(purrr)
@@ -23,30 +22,32 @@ library(sf)
 library(fields)
 library(usedist)
 library(vegan)
-library(reshape)
 library(dplyr)
 library(tidyr)
 library(tibble)
+source("Engel_novel_code.R") # from Engel et al. 2021, 10.1002/ecs2.3745
 
+# Data -------------------------------------------------------------------------
 
+landfrag_abundance <- read.csv("data/landfrag_37sset_abundance.csv")
+diversity_raref <- read.csv("processed_data/frag_cont_raref_2.csv")
 
-### For loops --- estimated alpha, beta and gamma = WITH SAMPLE COVERAGE ---------------------
+### For loops --- estimated alpha, beta and gamma = WITH SAMPLE COVERAGE -------
+## q = 0, q = 2 ---- running with all pairs only 
 
 gamma_of_2_frag_values = data.frame()
 gamma_of_2_cont_values = data.frame()
 
 frag_cont_comp = data.frame()
 
-frag_studies <- unique(frag_design_short$refshort)
-
-i = "Almeida-Gomes_2014"
+frag_studies <- unique(landfrag_abundance$refshort)
 
 for (i in frag_studies){
   print(i)
   
   # Subsetting main matrix by "ith" study
   
-  abund_mat = frag_cont_df %>% filter(refshort == i)
+  abund_mat = landfrag_abundance %>% filter(refshort == i)
   abund_vec = abund_mat %>% dplyr::select(fragment_plot_comb, patch_type, scientific_name, abundance)
   abund_vec2 = abund_vec %>% 
     group_by(fragment_plot_comb, patch_type, scientific_name) %>% 
@@ -150,11 +151,11 @@ for (i in frag_studies){
     pair = as.character(pair_df[1,])
     frag_pair_comm_all = frag_comm[rownames(frag_comm)%in%pair,]
     frag_pair_comm = frag_pair_comm_all[,colSums(frag_pair_comm_all)>0]
-    PIE_2_frag = calc_PIE(colSums(frag_pair_comm), ENS = FALSE)
+    PIE_2_frag = calc_PIE(colSums(frag_pair_comm))
     gamma_2_frag_q0 = estimateD(colSums(frag_pair_comm), q=0, datatype="abundance", base="coverage", level=gamma2_target) %>% pull(qD)
     gamma_2_frag_q2 = estimateD(colSums(frag_pair_comm), q=2, datatype="abundance", base="coverage", level=gamma2_target) %>% pull(qD)
     gamma_frag_df_temp = data.frame(refshort = i,
-                                    patch_type = "fragment",
+                                    patch_type = "fragmented",
                                     pair1 = pair[1],
                                     pair2 = pair[2],
                                     gamma_2_q0 = gamma_2_frag_q0,
@@ -172,7 +173,7 @@ for (i in frag_studies){
     pair = as.character(pair_df[1,])
     cont_pair_comm_all = cont_comm[rownames(cont_comm)%in%pair,]
     cont_pair_comm = cont_pair_comm_all[,colSums(cont_pair_comm_all)>0]
-    PIE_2_cont = calc_PIE(colSums(cont_pair_comm), ENS = FALSE) 
+    PIE_2_cont = calc_PIE(colSums(cont_pair_comm)) 
     gamma_2_cont_q0 = estimateD(colSums(cont_pair_comm), q=0, datatype="abundance", base="coverage", level=gamma2_target) %>% pull(qD)
     gamma_2_cont_q2 = estimateD(colSums(cont_pair_comm), q=2, datatype="abundance", base="coverage", level=gamma2_target) %>% pull(qD)
     gamma_cont_df_temp = data.frame(refshort = i,
@@ -212,12 +213,6 @@ for (i in frag_studies){
   beta_C_frag = as.numeric(beta_C(frag_data, target_coverage))
   beta_C_cont = as.numeric(beta_C(cont_data, target_coverage))
   
-  
-  # BetaC (based on target_coverage)
-  
-  # beta_C_frag2 = gamma_C_frag_q2 / alpha_C_frag_q2 
-  # beta_C_cont2 =  gamma_C_cont_q2 / alpha_C_cont_q2 
-  
   # Combine all metrics in a temporary file and rbind the results from each loop
   
   frag_cont_comp_temp = data.frame(refshort = i,
@@ -236,11 +231,16 @@ for (i in frag_studies){
 
 
 
+## save values from fragmented landscapes 
+
 gamma_of_2_frag_values %>% 
   dplyr::select(refshort, patch_type, gamma_2_q0, gamma_2_q2) %>% 
   pivot_longer(!c(refshort, patch_type), names_to = "diversity_index", values_to = "diversity_value") %>% 
   group_by(refshort, patch_type, diversity_index) %>% 
   dplyr::summarise(diversity_value = mean(diversity_value)) -> gamma_of_C2_frag_summ
+
+
+## save values from continuous landscapes 
 
 gamma_of_2_cont_values %>% 
   dplyr::select(refshort, patch_type, gamma_2_q0, gamma_2_q2) %>% 
@@ -248,13 +248,89 @@ gamma_of_2_cont_values %>%
   group_by(refshort, patch_type, diversity_index) %>% 
   dplyr::summarise(diversity_value = mean(diversity_value)) -> gamma_of_C2_cont_summ
 
-gamma_C <- rbind.data.frame(gamma_of_C2_frag_summ, gamma_of_C2_cont_summ) %>% 
-  arrange(refshort) 
+## combine them in the same data.frame
+gamma_C <- rbind.data.frame(gamma_of_C2_frag_summ, gamma_of_C2_cont_summ) 
+
+
+## Combine gamma (q = 0, q = 2) with coverage-based alpha (q = 0, q = 2) and beta 
 
 div_rareC_ABG <- rbind.data.frame(frag_cont_comp, gamma_C) %>% 
-  arrange(refshort)
+  ungroup() %>% 
+  dplyr::arrange(refshort)
 
-div_rareC_ABG$patch_type <- ifelse(div_rareC_ABG$patch_type == "fragment", "fragmented", div_rareC_ABG$patch_type)
+## export results to a csv file 
+## note, the data is still available in the folder processed_data
 
-div_rareC_ABG2 <- left_join(div_rareC_ABG, hamount_med, by = c("refshort", "patch_type"))
+# write.csv(div_rareC_ABG, "processed_data/div_rareC.csv")
+
+
+
+### rename to match other results from individual-based rarefaction to compare alpha and gamma diversity
+
+div_rareC_ABG %>% 
+  dplyr::filter(diversity_index != "beta_C") %>% 
+  mutate(
+    diversity_index_reformatted = case_when(
+      grepl("alphaC", diversity_index) ~ "alpha",
+      grepl("gamma_2", diversity_index) ~ "gamma"
+    ),
+    q_order = case_when(
+      grepl("C2|q2", diversity_index) ~ "q = 2",
+      grepl("C$|q0", diversity_index) ~ "q = 0"
+    )
+  ) %>%
+  dplyr::select(-diversity_index) %>%
+  rename(diversity_index = diversity_index_reformatted) %>% 
+  dplyr::arrange(refshort, patch_type, diversity_index) -> div_rareC_ABG_reform
+
+
+### correlation between individual- and coverage-based rarefaction (q = 0)
+
+### alpha diversity 
+
+div_rareC_ABG_reform %>% 
+  filter(q_order == "q = 0" & diversity_index == "alpha") -> alpha_q0_coverage
+
+diversity_raref %>% 
+  filter(q_order == "q = 0" & diversity_index == "alpha") -> alpha_q0_individual
+
+cor(alpha_q0_coverage$diversity_value, alpha_q0_individual$diversity_value)
+plot(alpha_q0_coverage$diversity_value, alpha_q0_individual$diversity_value)
+
+
+### gamma diversity 
+div_rareC_ABG_reform %>% 
+  filter(q_order == "q = 0" & diversity_index == "gamma") -> gamma_q0_coverage
+
+diversity_raref %>% 
+  filter(q_order == "q = 0" & diversity_index == "gamma") -> gamma_q0_individual
+
+cor(gamma_q0_coverage$diversity_value, gamma_q0_individual$diversity_value)
+plot(gamma_q0_coverage$diversity_value, gamma_q0_individual$diversity_value)
+
+
+
+### correlation between individual- and coverage-based rarefaction (q = 2)
+
+### alpha diversity 
+
+div_rareC_ABG_reform %>% 
+  filter(q_order == "q = 2" & diversity_index == "alpha") -> alpha_q2_coverage
+
+diversity_raref %>% 
+  filter(q_order == "q = 2" & diversity_index == "alpha") -> alpha_q2_individual
+
+cor(alpha_q2_coverage$diversity_value, alpha_q2_individual$diversity_value)
+plot(alpha_q2_coverage$diversity_value, alpha_q2_individual$diversity_value)
+
+
+### gamma diversity 
+div_rareC_ABG_reform %>% 
+  filter(q_order == "q = 2" & diversity_index == "gamma") -> gamma_q2_coverage
+
+diversity_raref %>% 
+  filter(q_order == "q = 2" & diversity_index == "gamma") -> gamma_q2_individual
+
+cor(gamma_q2_coverage$diversity_value, gamma_q2_individual$diversity_value)
+plot(gamma_q2_coverage$diversity_value, gamma_q2_individual$diversity_value)
 
